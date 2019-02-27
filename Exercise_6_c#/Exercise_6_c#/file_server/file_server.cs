46acb70052d4647ae2f54bd1c44fecfb89d96c67
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-02-27 08:30:57 +0100

    Exercise 6

---
+++ Exercise_6_c#/Exercise_6_c#/file_server/file_server.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcp
+{
+	class file_server
+	{
+		/// <summary>
+		/// The PORT
+		/// </summary>
+		const int PORT = 9000;
+		/// <summary>
+		/// The BUFSIZE
+		/// </summary>
+		const int BUFSIZE = 1000;
+
+        private int requestCount = 0;
+        private bool running = true;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="file_server"/> class.
+		/// Opretter en socket.
+		/// Venter på en connect fra en klient.
+		/// Modtager filnavn
+		/// Finder filstørrelsen
+		/// Kalder metoden sendFile
+		/// Lukker socketen og programmet
+ 		/// </summary>
+		private file_server ()
+        {
+            TcpListener serverSocket = new TcpListener(PORT);
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+
+            while (running)
+            {
+                requestCount++;
+                string returnText = LIB.readTextTCP(clientSocket.GetStream());
+                long fileSize = LIB.check_File_Exists(returnText);
+                if (fileSize != 0)
+                {
+                    LIB.writeTextTCP(clientSocket.GetStream(), fileSize.ToString());
+                }
+
+            }
+            serverSocket.Stop();
+            clientSocket.Close();
+        }
+
+		/// <summary>
+		/// Sends the file.
+		/// </summary>
+		/// <param name='fileName'>
+		/// The filename.
+		/// </param>
+		/// <param name='fileSize'>
+		/// The filesize.
+		/// </param>
+		/// <param name='io'>
+		/// Network stream for writing to the client.
+		/// </param>
+		private void sendFile (String fileName, long fileSize, NetworkStream io)
+		{
+			
+		}
+
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Server starts...");
+			new file_server();
+		}
+	}
+}
